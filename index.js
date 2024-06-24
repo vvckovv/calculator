88fdd4e2045105f18822a6362947dbc0b7d5a146
vvckovv <oloyedeope14@gmail.com>
2024-06-24 18:45:37 +0100

    added scripts to javascript, updated css stylesheet and updated html

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-index.js
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearToDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}
